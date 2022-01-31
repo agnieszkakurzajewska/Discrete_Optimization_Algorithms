f4bf09787e316c05747d12b5f5103cac8235cbbc
agnieszkakurzajewska <agnieszkakurzajewska@hotmail.com>
2022-01-31 21:02:42 +0100

    flow counted

--- 4/hypercubes.py
+++ 4/hypercubes.py
@@ -2,12 +2,20 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random
 import sys
+import math
+import numpy as np
 
-def printGraph(G, k):
-    if(k<10):
+def getK(G):
+    n = G.number_of_nodes()
+    k = math.log(n,2)
+    return int(k)
+
+def printGraph(G):
+    
+    if(getK(G)<10):
         pos=nx.spring_layout(G)
         nx.draw(G, pos, with_labels=True, node_size=5)
-        labels = nx.get_edge_attributes(G,'u')
+        labels = nx.get_edge_attributes(G,'c')
         nx.draw_networkx_edge_labels(G,pos, edge_labels=labels)
         plt.show()
 
@@ -23,15 +31,50 @@ def e(i):
     i_to_string = ''.join(map(str, i))
     return int(i_to_string, 2)
 
-def randomizeU(e):
+def randomizeC(e):
     l = max(H(e[0]), Z(e[0]), H(e[1]), Z(e[1]))
     return random.randint(1, 2**l)
 
-def addUToEdges(G):
+def addCFToEdges(G):
     for e in G.edges:
-        G.add_edge(e[0], e[1], u = randomizeU(e))
+        G.add_edge(e[0], e[1], c = randomizeC(e), f = 0)
     return G
 
-k = int(sys.argv[1])
-G = addUToEdges(nx.hypercube_graph(k))
-printGraph(G, k)
+def createGraph(k):
+    k = int(k)
+    G = nx.hypercube_graph(int(k))
+    G = addCFToEdges(G)
+    return G
+
+def createResidualGraph(G):
+    k = getK(G)
+    print(k)
+    Gf = nx.hypercube_graph(int(k))
+    return Gf
+
+def getHeaviestPath(G):
+    s = list(G.nodes)[0]
+    t = list(G.nodes)[G.number_of_nodes()-1]
+
+    max_flow = 0
+    best_path = None
+
+    for path in nx.all_simple_paths(G, source=s, target=t):
+         flow = 0
+         print(path[1])
+         for i in range(1, len(path)):
+            flow = flow + G[path[i-1]][path[i]]['c']
+            if flow>max_flow:
+                max_flow = flow
+                best_path = path
+    
+    print(best_path)
+    return best_path
+
+# main
+G = createGraph(sys.argv[1])
+Gf = createResidualGraph(G)
+getHeaviestPath(G)
+
+#print(list(G.nodes)[G.number_of_nodes()-1])
+printGraph(G)
